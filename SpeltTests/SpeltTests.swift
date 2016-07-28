c96a3213b9f836bf3f0d84903c96c26525b37d5f
Niels de Hoog <njdehoog@gmail.com>
2016-07-28 14:08:12 +0200

    Created new xcodeproj

---
+++ SpeltTests/SpeltTests.swift
@@ -0,0 +1,18 @@
+//
+//  SpeltTests.swift
+//  SpeltTests
+//
+//  Created by Niels de Hoog on 28/07/16.
+//
+//
+
+import XCTest
+@testable import Spelt
+
+class SpeltTests: XCTestCase {
+
+    func testSomething() {
+        XCTAssertNotNil(Site())
+    }
+    
+}
