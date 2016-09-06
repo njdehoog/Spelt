c6fab9e8e2d0d975996d15255c45241e1d2d6525
Niels de Hoog <njdehoog@gmail.com>
2016-09-06 16:46:54 +0200

    Build site before serving

--- Sources/CLI/Build.swift
+++ Sources/CLI/Build.swift
@@ -2,25 +2,27 @@ import SpeltKit
 import Commandant
 import Result
 
-struct BuildCommand: CommandType {
-    static let currentDirectoryPath = NSFileManager().currentDirectoryPath
-
-    struct Options: OptionsType {
-        let sourcePath: String
-        let destinationPath: String
-        
-        static func create(sourcePath: String) -> String -> Options {
-            return { destinationPath in
-                return Options(sourcePath: sourcePath.stringByStandardizingPath.absolutePath, destinationPath: destinationPath.stringByStandardizingPath.absolutePath);
-            }
-        }
-        
-        static func evaluate(m: CommandMode) -> Result<Options, CommandantError<SpeltError>> {
-            return create
-                <*> m <| Option(key: "source", defaultValue: BuildCommand.currentDirectoryPath, usage: "Source directory (defaults to ./)")
-                <*> m <| Option(key: "destination", defaultValue: BuildCommand.currentDirectoryPath.stringByAppendingPathComponent("_build"), usage: "Destination directory (defaults to ./_build)")
+struct PathOptions: OptionsType {
+    let sourcePath: String
+    let destinationPath: String
+    
+    static func create(sourcePath: String) -> String -> PathOptions {
+        return { destinationPath in
+            return PathOptions(sourcePath: sourcePath.stringByStandardizingPath.absolutePath, destinationPath: destinationPath.stringByStandardizingPath.absolutePath);
         }
     }
+    
+    static func evaluate(m: CommandMode) -> Result<PathOptions, CommandantError<SpeltError>> {
+        return create
+            <*> m <| Option(key: "source", defaultValue: BuildCommand.currentDirectoryPath, usage: "Source directory (defaults to ./)")
+            <*> m <| Option(key: "destination", defaultValue: BuildCommand.currentDirectoryPath.stringByAppendingPathComponent("_build"), usage: "Destination directory (defaults to ./_build)")
+    }
+}
+
+struct BuildCommand: CommandType {
+    typealias Options = PathOptions
+    
+    static let currentDirectoryPath = NSFileManager().currentDirectoryPath
     
     let verb = "build"
     let function = "Build your site"
--- Sources/CLI/Serve.swift
+++ Sources/CLI/Serve.swift
@@ -3,19 +3,19 @@ import Commandant
 import Result
 
 struct ServeCommand: CommandType {
+    typealias Options = PathOptions
+    
     let verb = "serve"
     let function = "Preview your site locally"
     
-    func run(options: NoOptions<SpeltError>) -> Result<(), SpeltError> {
-//        do {
-//        
-//        }
-//        catch {
-//            // FIXME: handle error
-//            return Result.Failure(SpeltError.defaultError)
-//        }
-        
-        SiteServer(directoryPath: NSFileManager().currentDirectoryPath).run()
+    func run(options: Options) -> Result<(), SpeltError> {
+        let result = BuildCommand().run(options)
+        switch result {
+        case .Success(_):
+            SiteServer(directoryPath: options.destinationPath).run()
+        case .Failure(_):
+            return result
+        }
         
         return Result.Success()
     }
