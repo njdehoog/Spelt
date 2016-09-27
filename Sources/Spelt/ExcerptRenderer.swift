f0242140caf6984235ec19b1bc5325f9c671e9bf
Niels de Hoog <njdehoog@gmail.com>
2016-09-27 15:49:02 +0200

    Perform anchored search

--- Sources/Spelt/ExcerptRenderer.swift
+++ Sources/Spelt/ExcerptRenderer.swift
@@ -10,7 +10,7 @@ struct ExcerptRenderer: Renderer {
             
             // match first double newline and return everything before that
             let paragraphRegex = try NSRegularExpression(pattern: "^(.*?)(\n\n|$)", options: [.caseInsensitive, .dotMatchesLineSeparators])
-            if let excerpt = paragraphRegex.stringForFirstMatch(post.contents, options: NSRegularExpression.MatchingOptions(), rangeAtIndex: 1) {
+            if let excerpt = paragraphRegex.stringForFirstMatch(post.contents, options: .anchored, rangeAtIndex: 1) {
                 // render markdown in excerpt
                 let rendered = try MarkdownConverter().convert(excerpt)
                 post.metadata["excerpt"] = .string(rendered)
